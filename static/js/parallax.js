e5c4a1b4b502588a49b0a55e87e62fb180a49ae8
AndeyGordeev <prolodgy@yandex.ru>
2018-05-29 10:22:49 +0300

    delete parallax from phone and tablet

--- static/js/parallax.js
+++ static/js/parallax.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
     $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this); // создаем объект
-        var $window = $(window);
-        $window.scroll(function() {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент 
-            // Присваиваем значение background-position
-            var coords = 'center '+ yPos + 'px';
-            // Создаем эффект Parallax Scrolling
-            $bgobj.css({ backgroundPosition: coords });
-        });
+        if ($('body').width() > 990) 
+        {
+            var $bgobj = $(this); // создаем объект
+            var $window = $(window);
+            $window.scroll(function() {
+                var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент 
+                // Присваиваем значение background-position
+                var coords = 'center '+ yPos + 'px';
+                // Создаем эффект Parallax Scrolling
+                $bgobj.css({ backgroundPosition: coords });
+            });
+        };
     });
 });
