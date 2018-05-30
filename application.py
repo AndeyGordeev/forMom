b9e3c29e7e96e9a717fc9095e78c754b15f93185
AndeyGordeev <prolodgy@yandex.ru>
2018-05-30 15:44:57 +0300

    Add posts and link

--- application.py
+++ application.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/post1')
+def post1():
+	return render_template('post1.html')
+
+@app.route('/post2')
+def post2():
+	return render_template('post2.html')
+
+@app.route('/post3')
+def post3():
+	return render_template('post3.html')
+
+@app.route('/post4')
+def post4():
+	return render_template('post4.html')
+
+@app.route('/post5')
+def post5():
+	return render_template('post5.html')
