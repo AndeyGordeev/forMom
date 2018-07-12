aab479ec77d647f2e0ef6ba82e669c22e40354ea
AndeyGordeev <prolodgy@yandex.ru>
2018-07-12 23:20:14 +0300

    Delete parralax

--- static/js/parallax.js
+++ static/js/parallax.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
-    $('.postImage[data-type="backgroundPost"]').each(function(){
-        if ($('body').width() > 990) 
-        {
-            var $bgobj = $(this); // создаем объект
-            var $window = $(window);
-            $window.scroll(function() {
-                var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент 
-                // Присваиваем значение background-position
-                var coords = 'center '+ yPos + 'px';
-                // Создаем эффект Parallax Scrolling
-                $bgobj.css({ backgroundPosition: coords });
-            });
-        };
-    });
+// $(document).ready(function(){
+//     $('.postImage[data-type="backgroundPost"]').each(function(){
+//         if ($('body').width() > 990) 
+//         {
+//             var $bgobj = $(this); // создаем объект
+//             var $window = $(window);
+//             $window.scroll(function() {
+//                 var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент 
+//                 // Присваиваем значение background-position
+//                 var coords = 'center '+ yPos + 'px';
+//                 // Создаем эффект Parallax Scrolling
+//                 $bgobj.css({ backgroundPosition: coords });
+//             });
+//         };
+//     });
 
-    $('.back[data-type="background"]').each(function(){
-        if ($('body').width() > 990) 
-        {
-            var $bgobj = $(this); // создаем объект
-            var $window = $(window);
-            $window.scroll(function() {
-                var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент 
-                // Присваиваем значение background-position
-                var coords = 'center '+ yPos * 1 + 'px';
-                // Создаем эффект Parallax Scrolling
-                $bgobj.css({ backgroundPosition: coords });
-            });
-        };
-    });
+//     $('.back[data-type="background"]').each(function(){
+//         if ($('body').width() > 990) 
+//         {
+//             var $bgobj = $(this); // создаем объект
+//             var $window = $(window);
+//             $window.scroll(function() {
+//                 var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент 
+//                 // Присваиваем значение background-position
+//                 var coords = 'center '+ yPos * 1 + 'px';
+//                 // Создаем эффект Parallax Scrolling
+//                 $bgobj.css({ backgroundPosition: coords });
+//             });
+//         };
+//     });
 
 });
